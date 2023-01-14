1a0c8ae273ad8e9865dc7c87169212def84a8d6e
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-01-14 22:13:28 +0900

    📄 :: MyInfoSection 생성

--- app/src/main/java/com/highthon/dokgodie_android/presentation/feature_mypage/MyPageScreen.kt
+++ app/src/main/java/com/highthon/dokgodie_android/presentation/feature_mypage/MyPageScreen.kt
@@ -1,16 +1,22 @@
 package com.highthon.dokgodie_android.presentation.feature_mypage
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.highthon.dokgodie_android.presentation.component.TopBar
+import com.highthon.dokgodie_android.presentation.feature_mypage.componenet.MyInfoSection
+import com.highthon.dokgodie_android.presentation.ui.theme.White500
 
 @Composable
 fun MyPageScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(White500)) {
         TopBar(text = "마이 페이지", isNeedBackPressButton = false)
+        MyInfoSection()
     }
 }
 
---
+++ app/src/main/java/com/highthon/dokgodie_android/presentation/feature_mypage/componenet/MyInfoSection.kt
@@ -0,0 +1,8 @@
+package com.highthon.dokgodie_android.presentation.feature_mypage.componenet
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MyInfoSection() {
+
+}
